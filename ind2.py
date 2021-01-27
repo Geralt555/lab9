49af7eb128abb3f51f3e13776d32444f4580f3a0
Geralt555 <70748374+Geralt555@users.noreply.github.com>
2021-01-27 12:50:00 +0300

    Update ind2.py

--- ind2.py
+++ ind2.py
@@ -1,113 +1,87 @@
-# !/usr/bin/env python3
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
+# Вариант 8.Использовать словарь, содержащий следующие ключи: название пункта назначения; номер
+# поезда; время отправления. Написать программу, выполняющую следующие действия:
+# ввод с клавиатуры данных в список, состоящий из словарей заданной структуры; записи должны
+# быть упорядочены по номерам поездов;
+# вывод на экран информации о поезде, номер которого введен с клавиатуры; если таких поездов нет,
+# выдать на дисплей соответствующее сообщение.
 
 import sys
 import json
 
-# Использовать словарь, содержащий следующие ключи: фамилия, имя; номер телефона;
-# дата рождения (список из трех чисел). Написать программу, выполняющую следующие
-# действия: ввод с клавиатуры данных в список, состоящий из словарей заданной структуры;
-# записи должны быть размещены по алфавиту; вывод на экран информации о людях, чьи
-# дни рождения приходятся на месяц, значение которого введено с клавиатуры; если таких
-# нет, выдать на дисплей соответствующее сообщение.
 
 if __name__ == '__main__':
-    workers = []
+
+    trains = []
+
     while True:
-        command = input("Enter command> ").lower()
+        command = input(">>> ").lower()
 
-        if command == "exit":
+        if command == 'exit':
             break
+        elif command == 'add':
+            name = input("Название пункта назначения: ")
+            num = int(input("Номер поезда: "))
+            time = input("Время отправления: ")
 
-        elif command == "add":
-            las_name = str(input("Enter last name>  "))
-            name = str(input("Enter first name> "))
-            tel = int(input("Enter phone> +"))
-            date = list(map(int, input("Enter birthdate separated by space> ").split(" ")))
-
-            temp = {
-                'las_name': las_name,
+            train = {
                 'name': name,
-                'tel': tel,
-                'date': date
+                'num': num,
+                'time': time,
             }
-            workers.append(temp)
-            # Отсортировать список в случае необходимости.
-            if len(workers) > 1:
-                workers.sort(key=lambda item: item.get('las_name', ''))
 
-        elif command == "list":
-            line = "+-{}-+-{}-+-{}-+-{}-+-{}-+".format(
+            trains.append(train)
+            if len(trains) > 1:
+                trains.sort(key=lambda item: item.get('num', 0))
+
+        elif command == 'list':
+            line = '+-{}-+-{}-+-{}-+-{}-+'.format(
                 '-' * 4,
-                '-' * 15,
-                '-' * 15,
+                '-' * 30,
                 '-' * 20,
-                '-' * 20
+                '-' * 17
             )
             print(line)
             print(
-                "| {:^4} | {:^15} | {:^15} | {:^20} | {:^20} |".format(
+                '| {:^4} | {:^30} | {:^20} | {:^17} |'.format(
                     "№",
-                    "Фамилия",
-                    "Имя",
-                    "Телефон",
-                    "Дата рождения"
+                    "Пункт назначения",
+                    "Номер поезда",
+                    "Время отправления"
                 )
             )
             print(line)
-            for idx, worker in enumerate(workers, 1):
+
+            for idx, train in enumerate(trains, 1):
                 print(
-                    '| {:>4} | {:<15} | {:<15} | {:>20} | {:^20} |'.format(
+                    '| {:>4} | {:<30} | {:<20} | {:>17} |'.format(
                         idx,
-                        worker.get('las_name', ''),
-                        worker.get('name', ''),
-                        worker.get('tel', 0),
-                        ".".join(map(str, worker.get('date')))
+                        train.get('name', ''),
+                        train.get('num', 0),
+                        train.get('time', '')
                     )
                 )
+
             print(line)
 
-        elif command == "task":
-            check = list(map(int, input("Enter birthdate separated by space> ").split(" ")))
-            task_list = []
-            iz = 0
-            for worker in workers:
-                if ''.join(map(str, worker.get('date'))) == ''.join(map(str, check)):
-                    task_list.append(worker)
-                    iz += 1
+        elif command.startswith('select '):
 
-            if iz == 0:
-                print("Workers not found")
-            else:
-                line = "+-{}-+-{}-+-{}-+-{}-+-{}-+".format(
-                    '-' * 4,
-                    '-' * 15,
-                    '-' * 15,
-                    '-' * 20,
-                    '-' * 20
-                )
-                print(line)
-                print(
-                    "| {:^4} | {:^15} | {:^15} | {:^20} | {:^20} |".format(
-                        "№",
-                        "Фамилия",
-                        "Имя",
-                        "Телефон",
-                        "Дата рождения"
-                    )
-                )
-                print(line)
-                for idx, worker in enumerate(task_list, 1):
-                    print(
-                        '| {:>4} | {:<15} | {:<15} | {:>20} | {:^20} |'.format(
-                            idx,
-                            worker.get('las_name', ''),
-                            worker.get('name', ''),
-                            worker.get('tel', 0),
-                            ".".join(map(str, worker.get('date')))
-                        )
-                    )
-                print(line)
+            parts = command.split(' ', maxsplit=2)
+
+            number = int(parts[1])
+
+            count = 0
+            for train in trains:
+                if train.get('num') == number:
+                    count += 1
+                    print('Номер поезда:', train.get('num', 0))
+                    print('Пункт назначения:', train.get('name', ''))
+                    print('Время отправления:', train.get('time', ''))
+
+            if count == 0:
+                print("Таких поездов нет!")
 
         elif command.startswith('load '):
             # Разбить команду на части для выделения имени файла.
@@ -115,7 +89,7 @@ if __name__ == '__main__':
 
             # Прочитать данные из файла JSON.
             with open(parts[1], 'r') as f:
-                workers = json.load(f)
+                trains = json.load(f)
 
         elif command.startswith('save '):
             # Разбить команду на части для выделения имени файла.
@@ -123,17 +97,16 @@ if __name__ == '__main__':
 
             # Сохранить данные в файл JSON.
             with open(parts[1], 'w') as f:
-                json.dump(workers, f)
+                json.dump(trains, f)
 
         elif command == 'help':
-            # Вывести справку о работе с программой.
             print("Список команд:\n")
-            print("add - добавить работника;")
-            print("list - вывести список работников;")
-            print("task - вывести сотрудников определенной даты рождения")
+            print("add - добавить поезд;")
+            print("list - вывести список поездов;")
+            print("select <номер поезда> - запросить информацию о выбранном поезде;")
+            print("help - отобразить справку;")
             print("load <имя файла> - загрузить данные из файла;")
             print("save <имя файла> - сохранить данные в файл;")
-            print("exit - выход из программы;")
-
+            print("exit - завершить работу с программой.")
         else:
-            print("Неизвестная команда {command}", file=sys.stderr)
+            print(f"Неизвестная команда {command}", file=sys.stderr)
