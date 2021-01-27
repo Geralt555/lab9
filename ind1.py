c93c4eff0b9180ea6b72abfee1425f88d64812ac
Geralt555 <70748374+Geralt555@users.noreply.github.com>
2021-01-27 13:27:39 +0300

    Update ind1.py

--- ind1.py
+++ ind1.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#Написать программу, которая считывает текст из файла, находит самое длинное слово и
-#определяет, сколько раз оно встретилось в тексте.
-
-import re
+# Написать программу, которая считывает из текстового файла три предложения и выводит их
+# в обратном порядке.
 
 with open('input.txt', encoding='utf-8') as f:
     words = f.read().strip()
+print(words)
 
-arr = [word for word in re.split('[ ,.\n]', words) if word]
-max_len_word = sorted(arr, key=len)[-1]
-count_word = arr.count(max_len_word)
-
-print('Самое длинное слово: "{max_len_word}"\n{count_word} раз встречается в тексте')
+i = words.find('.')
+b = words[:i + 1]
+words = words[i + 2:]
+i = words.find('.')
+c = words[:i + 1]
+words = words[i + 2:]
+print(words, c, b)
