05d24e4f6132f6691c1912f443e44989c2982060
Geralt555 <70748374+Geralt555@users.noreply.github.com>
2021-01-28 14:18:13 +0300

    Update ind1.py

--- ind1.py
+++ ind1.py
@@ -4,14 +4,15 @@
 # Написать программу, которая считывает из текстового файла три предложения и выводит их
 # в обратном порядке.
 
-with open('input.txt', encoding='utf-8') as f:
-    words = f.read().strip()
-print(words)
+if __name__ == '__main__':
+    with open('input.txt', encoding='utf-8') as f:
+        words = f.read().strip()
+    print(words)
 
-i = words.find('.')
-b = words[:i + 1]
-words = words[i + 2:]
-i = words.find('.')
-c = words[:i + 1]
-words = words[i + 2:]
-print(words, c, b)
+    i = words.find('.')
+    b = words[:i + 1]
+    words = words[i + 2:]
+    i = words.find('.')
+    c = words[:i + 1]
+    words = words[i + 2:]
+    print(words, c, b)
